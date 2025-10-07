c4cfac853d83deca69a9cd9dcc748b9a475d54a8
swediRoben <101991836+swediRoben@users.noreply.github.com>
2025-10-07 14:15:39 +0200

    Update db.js
    public url

--- db.js
+++ db.js
@@ -1,13 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-    }
-);
+// Utiliser la chaîne de connexion complète de Railway
+const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
+    dialect: 'mysql',
+    logging: console.log, // facultatif : pour voir les requêtes SQL
+});
 
 module.exports = sequelize;
